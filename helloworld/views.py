5737b8695a6043ac5be6c10b697d37f55176cb24
leeadammmit <30483333+leeadammmit@users.noreply.github.com>
2018-03-09 14:25:24 +1100

    final checkin

--- helloworld/views.py
+++ helloworld/views.py
@@ -7,3 +7,5 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
+
+    
